dbfc89b532998676fc25c09b70fc417297f5fc04
Joey <malvinniguitar@gmail.com>
2021-04-14 16:22:57 -0700

    v1.4.0: Trim unused years from data and push to Heroku

--- app.js
+++ app.js
@@ -10,17 +10,65 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/:user/:format', (req, res, next) => {
   const { format, user } = req.params;
+  const FORMATS = ['activity', 'count']
+  if (typeof user !== 'string') return next ({ 
+    status: 400,
+    message: 'Invalid type of user \'' + typeof user + '\'. Expected to be of type \'String\'.'
+  });
+  if (typeof format !== 'string') return next ({ 
+    status: 400,
+    message: 'Invalid type of format \'' + typeof user + '\'. Expected format to be of type \'String\'.'
+  })
+  if (!FORMATS.includes(format)) return next ({ 
+    status: 400,
+    message: 'Invalid format ' + JSON.stringify(format) +'. Expected one of ' + FORMATS.join(' or ') + '.'
+  })
+  const url = `https://www.github.com/${user}`;
 
-  // Render 400 if invalid format given
-  const VALID_FORMATS = ['activity', 'count'];
-  if (!VALID_FORMATS.includes(format)) {
-    return next({ 
-      status: 400, 
-      message: `Format must be one of ${JSON.stringify(VALID_FORMATS)}`
+  request.get(url, (err, response, body) => {
+    // Return error if request had an error
+    if (err) return next(err);
+
+    // Return 404 if user not found
+    if (response.statusCode === 404) return next({
+      status: 404,
+      message: `User '${user}' not found`
     });
-  }
 
-  renderData(res, req, next)
+    // Parse github profile page
+    const $ = cheerio.load(body);
+    let final = {};
+    $('rect').get().reduce((data, rect) => {
+      // Parse contributions value
+      const value = (() => {
+        const count = $(rect).data('count');
+        if (format === 'activity') return count > 0;
+        if (format === 'count') return count;
+      })();
+
+      // Parse contributions date
+      if (!$(rect).data('date')) return;
+      const [year, month, day] = $(rect).data('date').split('-').map(dateNum => parseInt(dateNum));
+      if (!final[year]) final[year] = {};
+      if (!final[year][month]) final[year][month] = {}
+      final[year][month][day] = value
+    }, {})
+    let trimmed = {};
+    for (let i = 0; i < Object.keys(final).length; i++) {
+      let current = Object.values(final)[i];
+      let currentYear = Object.keys(final)[i];
+      let values = false;
+      for (let [date, value] of Object.entries(Object.values(Object.entries(current)[i])[1])) {
+        if (value !== 0 && value !== false) {
+          values = true;
+          break;
+        };
+      };
+
+      if (values) trimmed[currentYear] = current;
+    }
+    res.json(trimmed)
+  })
 });
 
 // catch 404 and forward to error handler
@@ -36,48 +84,11 @@ app.use(function(err, req, res, next) {
   res.json({ message: err.message });
 });
 
-function renderData (res, req, next) {
-  return new Promise((resolve, reject) => {
-    const { format, user } = req.params;
-    const url = `https://www.github.com/${user}`;
-
-    request.get(url, (err, response, body) => {
-      // Return error if request had an error
-      if (err) return next(err);
-  
-      // Return 404 if user not found
-      if (response.statusCode === 404) return next({
-        status: 404,
-        message: `User '${user}' not found`
-      });
-  
-      // Parse github profile page
-      const $ = cheerio.load(body);
-      let final = {};
-      $('rect').get().reduce((data, rect) => {
-        // Parse contributions value
-        const value = (() => {
-          const count = $(rect).data('count');
-          if (format === 'activity') return count > 0;
-          if (format === 'count') return count;
-        })();
-  
-        // Parse contributions date
-        if (!$(rect).data('date')) return;
-        const [year, month, day] = $(rect).data('date').split('-').map(dateNum => parseInt(dateNum));
-        if (!final[year]) final[year] = {};
-        if (!final[year][month]) final[year][month] = {}
-        final[year][month][day] = value
-      }, {})
-      res.json(final);
-      resolve(final);
-    })
-  })
-}
-
 function getGithubContributions (user, format) {
   return new Promise((resolve, reject) => {
     const FORMATS = ['activity', 'count']
+    if (typeof user !== 'string') reject ('Invalid type of user \'' + typeof user + '\'. Expected to be of type \'String\'.');
+    if (typeof format !== 'string') reject ('Invalid type of format \'' + typeof user + '\'. Expected format to be of type \'String\'.')
     if (!FORMATS.includes(format)) reject('Invalid format ' + JSON.stringify(format) +'. Expected one of ' + FORMATS.join(' or '));
     const url = `https://www.github.com/${user}`;
 
@@ -109,7 +120,21 @@ function getGithubContributions (user, format) {
         if (!final[year][month]) final[year][month] = {}
         final[year][month][day] = value
       }, {})
-      resolve(final);
+      let trimmed = {};
+      for (let i = 0; i < Object.keys(final).length; i++) {
+        let current = Object.values(final)[i];
+        let currentYear = Object.keys(final)[i];
+        let values = false;
+        for (let [date, value] of Object.entries(Object.values(Object.entries(current)[i])[1])) {
+          if (value !== 0 && value !== false) {
+            values = true;
+            break;
+          };
+        };
+
+        if (values) trimmed[currentYear] = current;
+      }
+      resolve(trimmed);
     })
   })
 }
